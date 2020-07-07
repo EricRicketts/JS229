a5b713d28baac97e4b8a392c20ddc001f193d98e
EricRicketts <eric_ricketts@me.com>
2020-07-06 21:35:10 -0400

    fix todoList test

--- test/todo_list.test.js
+++ test/todo_list.test.js
@@ -1,42 +1,42 @@
 import { TodoList } from "../code/todo_list";
 
 describe('TodoList', function () {
-  let expected, results;
+  let todoData1, todoData2, todoData3, todoData4, todoSet, todoList, expected, results;
+  beforeAll(() => {
+    todoData1 = {
+      title: 'Buy Milk',
+      month: '1',
+      year: '2017',
+      description: 'Milk for baby',
+    };
+
+    todoData2 = {
+      title: 'Buy Apples',
+      month: '',
+      year: '2017',
+      description: 'An apple a day keeps the doctor away',
+    };
+
+    todoData3 = {
+      title: 'Buy chocolate',
+      month: '1',
+      year: '',
+      description: 'For the cheat day',
+    };
+
+    todoData4 = {
+      title: 'Buy Veggies',
+      month: '',
+      year: '',
+      description: 'For the daily fiber needs',
+    };
+
+    todoSet = [todoData1, todoData2, todoData3, todoData4];
+    todoList = new TodoList(todoSet);
+  });
+
   describe('Operations on valid data', function () {
-    let todoData1, todoData2, todoData3, todoData4, todoSet, todoList, foundTodo;
-    beforeEach(() => {
-      todoData1 = {
-        title: 'Buy Milk',
-        month: '1',
-        year: '2017',
-        description: 'Milk for baby',
-      };
-
-      todoData2 = {
-        title: 'Buy Apples',
-        month: '',
-        year: '2017',
-        description: 'An apple a day keeps the doctor away',
-      };
-
-      todoData3 = {
-        title: 'Buy chocolate',
-        month: '1',
-        year: '',
-        description: 'For the cheat day',
-      };
-
-      todoData4 = {
-        title: 'Buy Veggies',
-        month: '',
-        year: '',
-        description: 'For the daily fiber needs',
-      }; 
-      
-      todoSet = [todoData1, todoData2, todoData3, todoData4]
-      todoList = new TodoList(todoSet);
-    });
-
+    let foundTodo;
     it('should return a todo list object with 4 todo items', function () {
       expect(todoList.numberOfTodos()).toBe(4);
     });
@@ -52,7 +52,7 @@ describe('TodoList', function () {
       expect(results).toEqual(todoData3);
     });
 
-    it.skip('should add a new todo item', function () {
+    it('should add a new todo item', function () {
       let todoData5 = {
         title: 'Buy novel',
         month: '4',
