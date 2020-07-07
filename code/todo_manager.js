9a1af04c58d10ff61cd6ac352e222bc5ae7949c1
EricRicketts <eric_ricketts@me.com>
2020-07-07 06:02:47 -0400

    todo manager

--- code/todo_manager.js
+++ code/todo_manager.js
@@ -2,8 +2,22 @@ function TodoManger(todoList) {
   this.todoList = todoList;
 }
 
-TodoManger.prototype.all = function() {
-
+TodoManger.prototype.allTodos = function() {
+  return this.todoList.getAllTodos();
 }
 
+TodoManger.prototype.allCompletedTodos = function() {
+  let allTodos = this.allTodos();
+  return allTodos.filter((todoItem) => todoItem.completed);
+}
+
+TodoManger.prototype.findCompletedByMonthAndYear = function(month, year) {
+  let foundTodos = this.findByMonthAndYear(month, year);
+  return foundTodos.filter((todoItem) => todoItem.completed);
+}
+
+TodoManger.prototype.findByMonthAndYear = function(month, year) {
+  let allTodos = this.allTodos();
+  return allTodos.filter((todoItem) => todoItem.isWithinMonthYear(month, year));
+}
 export { TodoManger };
--- test/todo_app.test.js
+++ test/todo_app.test.js
@@ -2,7 +2,7 @@ import { TodoList, TodoManger  } from "../code/todoApp";
 
 describe('todoApp', function () {
   let todoData1, todoData2, todoData3, todoData4, todoData5, todoData6, todoSet, todoList,
-    todoManager, expected, results;
+    todoManager, foundTodos, expected, results;
   beforeAll(() => {
     todoData1 = {
       title: 'Buy Milk',
@@ -13,7 +13,7 @@ describe('todoApp', function () {
 
     todoData2 = {
       title: 'Buy Apples',
-      month: '',
+      month: '1',
       year: '2017',
       description: 'An apple a day keeps the doctor away',
     };
@@ -52,8 +52,30 @@ describe('todoApp', function () {
   });
 
   it('todo manager returns all todo objects in an array', function () {
-    results = todoManager.all();
+    results = todoManager.allTodos();
     expect(results.length).toBe(6);
   });
 
+  it('todo manager returns all completed todo objects in an array', function () {
+    todoList.updateTodoItem(0, { completed: true });
+    todoList.updateTodoItem(2, { completed: true });
+    results = todoManager.allCompletedTodos();
+    expect(results.length).toBe(2);
+    expected = [0, 2];
+    expect([results[0].id, results[1].id]).toEqual(expected);
+  });
+
+  it('todo manager returns a list of todos based on month and year criteria', function () {
+    foundTodos = todoManager.findByMonthAndYear('1', '2017');
+    expect(foundTodos.length).toBe(2);
+    results = [foundTodos[0].id, foundTodos[1].id];
+    expected = [0, 1];
+    expect(results).toEqual(expected);
+  });
+
+  it('todo manager returns a list of todos based on monty, year, and completed criteria', function () {
+    foundTodos = todoManager.findCompletedByMonthAndYear('1', '2017');
+    expect(foundTodos.length).toBe(1);
+    expect(foundTodos[0].id).toBe(0);
+  });
 });
